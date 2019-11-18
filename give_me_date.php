6c6b423c924cc6428c45107544c4384ea62f3915
Ravi Bastola <bastola.ravi@hotmail.com>
2019-11-18 10:09:12 +0545

    updated typo

--- give_me_date.php
+++ give_me_date.php
@@ -53,7 +53,7 @@ foreach ($nepaliDateData as $date) {
               $date['month'] = $months[$date['month']];
               break;
         }
-        $monthNumber = empty($monthIndex)?array_search($date['month'], $months):$monthIndex;
+        $monthNumber = empty($monthNumber)?array_search($date['month'], $months):$monthIndex;
         $sql = /**@lang sql **/
         "insert into `monthly_data` (
           nepali_month,
